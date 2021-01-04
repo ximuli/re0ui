a752b81ccf0e4a46be19d30a82bfa443cb175c34
ximuli <ximuli666@163.com>
2021-01-04 21:32:30 +0800

    完善Button组件theme属性和size属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,10 +1,28 @@
 <template>
+<div>Button示例</div>
+<h1>示例1</h1>
 <div>
   <Button @click="onClick" @focus="onFocus" @mouseover="onMouseover">你好</Button>
   <Button theme="button">你好</Button>
   <Button theme="text">你好</Button>
   <Button theme="link">你好</Button>
   <div class="item" v-for="(item, index) in 5" :key="index">ButtonDemo</div>
+</div>
+<h1>示例2</h1>
+<div>
+  <Button size="big">你好</Button>
+  <Button>你好</Button>
+  <Button size="small">你好</Button>
+</div>
+<div>
+  <Button theme="link" size="big">你好</Button>
+  <Button theme="link">你好</Button>
+  <Button theme="link" size="small">你好</Button>
+</div>
+<div>
+  <Button theme="text" size="big">你好</Button>
+  <Button theme="text">你好</Button>
+  <Button theme="text" size="small">你好</Button>
 </div>
 </template>
 
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,16 +1,32 @@
 <template>
-  <button class="re0-button" :class="{ [`re0-theme-${theme}`]: theme }">
+  <button class="re0-button" :class="classes">
     <slot></slot>
   </button>
 </template>
 
 <script>
+import { computed } from 'vue'
 export default {
   props: {
     theme: {
       type: String,
       default: 'button'
+    },
+    size: {
+      type: String,
+      default: 'normal'
     }
+  },
+  setup(props) {
+    const { theme, size } = props
+    const classes = computed(() => {
+      return {
+        [`re0-theme-${theme}`]: theme,
+        [`re0-size-${size}`]: size
+      }
+    })
+
+    return { classes }
   }
 }
 </script>
@@ -40,6 +56,35 @@ $radius: 4px;
   }
   &::-moz-focus-inner {
     border: 0;
+  }
+
+  &.re0-theme-text {
+    border: none;
+    box-shadow: none;
+    color: #333;
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+
+  &.re0-theme-link {
+    border: none;
+    box-shadow: none;
+    color: $colorEmilia;
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+
+  &.re0-size-big {
+    font-size: 24px;
+    height: 48px;
+    padding: 0 16px;
+  }
+  &.re0-size-small {
+    font-size: 12px;
+    height: 20px;
+    padding: 0 4px;
   }
 }
 </style>

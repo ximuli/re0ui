2e6a4c211769738e5b412cde4af4af52fefabc68
ximuli <ximuli666@163.com>
2021-01-21 22:07:58 +0800

    添加可滑动的下划线

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-  <Tabs>
+  <Tabs v-model:selected="select">
     <Tab title="导航1">内容1</Tab>
     <Tab title="导航2">内容2</Tab>
   </Tabs>
@@ -11,10 +11,15 @@
 <script>
 import Tabs from '../lib/Tabs/Tabs.vue'
 import Tab from '../lib/Tabs/Tab.vue'
+import { ref } from 'vue'
 
 export default {
   components: {
     Tabs, Tab
+  },
+  setup() {
+    const select = ref('导航2')
+    return { select }
   }
 }
 </script>
--- src/lib/Tabs/Tabs.vue
+++ src/lib/Tabs/Tabs.vue
@@ -1,22 +1,47 @@
 <template>
   <div class="re0-tabs">
-    <div class="re0-tabs-nav">
-      <div class="re0-tabs-nav-item" v-for="(title, index) in titles" :key="index">
+    <div class="re0-tabs-nav" ref="container">
+      <div class="re0-tabs-nav-item" :class="{ selected: title === selected }"
+        v-for="(title, index) in titles" :key="index"
+        :ref="el => { if (el) navItems[index] = el }" @click="onSelect(title)">
         {{ title }}
       </div>
+      <div ref="indicator" class="re0-tabs-nav-indicator"></div>
     </div>
     <div class="re0-tabs-content">
-      <component v-for="(item, index) in defaults" :key="index" :is="item"></component>
+      <component class="re0-tabs-content-item"
+        :class="{ selected: item.props.title === selected }"
+        v-for="(item, index) in defaults" :key="index" :is="item"></component>
     </div>
   </div>
 </template>
 
 <script lang="ts">
+import { onMounted, onUpdated, ref } from 'vue'
 import Tab from './Tab.vue'
 
 export default {
+  props: {
+    selected: {
+      type: String
+    }
+  },
   setup(props, context) {
-    console.log(context.slots.default())
+    const navItems = ref<HTMLDivElement[]>([])
+    const indicator = ref<HTMLDivElement>(null)
+    const container = ref<HTMLDivElement>(null)
+    const getTabBorder = (() => {
+      const divs = navItems.value
+      const result = divs.filter(div => div.classList.contains('selected'))[0]
+      const { width } = result.getBoundingClientRect()
+      indicator.value.style.width = width + 'px'
+      console.log('container', container.value)
+      const { left: containerLeft } = container.value.getBoundingClientRect()
+      const { left: resultLeft } = result.getBoundingClientRect()
+      indicator.value.style.left = resultLeft - containerLeft + 'px'
+    })
+    onMounted(getTabBorder)
+    onUpdated(getTabBorder)
     const defaults = context.slots.default()
     defaults.forEach(tag => {
       if (tag.type !== Tab) {
@@ -26,7 +51,11 @@ export default {
     const titles = defaults.map(tag => {
       return tag.props.title
     })
-    return { defaults, titles }
+
+    const onSelect = (title: string) => {
+      context.emit('update:selected', title)
+    }
+    return { defaults, titles, onSelect, navItems, indicator, container }
   }
 }
 </script>
@@ -39,6 +68,7 @@ export default {
     display: flex;
     color: #333;
     border-bottom: 1px solid #d9d9d9;
+    position: relative;
     .re0-tabs-nav-item {
       padding: 8px 0;
       margin: 0 16px;
@@ -50,10 +80,25 @@ export default {
         color: $colorEmilia;
       }
     }
+    .re0-tabs-nav-indicator {
+      position: absolute;
+      height: 3px;
+      background: $colorEmilia;
+      left: 0;
+      bottom: -1px;
+      transition: left .3s ease;
+      // width: 100px;
+    }
   }
 
   .re0-tabs-content {
     padding: 8px 0;
+    .re0-tabs-content-item {
+      display: none;
+      &.selected {
+        display: block;
+      }
+    }
   }
 }
 </style>

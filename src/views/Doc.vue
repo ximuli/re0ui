819789be572981361357ae84a6510ccaba4d29f3
ximuli <ximuli666@163.com>
2021-04-07 11:38:19 +0800

    样式优化

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,47 +1,44 @@
 <template>
-<Topnav />
-<div class="content">
-  <aside class="aside" v-if="asideVisible" style="border: 1px solid green;">
-    <ol class="aside-list">
-      <li>
-        <router-link to="/doc/switch">Switch组件</router-link>
-      </li>
-      <li>
-        <router-link to="/doc/button">Button 组件</router-link>
-      </li>
-      <li>
-        <router-link to="/doc/dialog">Dialog 组件</router-link>
-      </li>
-      <li>
-        <router-link to="/doc/tabs">Tabs 组件</router-link>
-      </li>
-    </ol>
-  </aside>
-  <main class="main" style="border: 1px solid blue;">
-    <router-view></router-view>
-  </main>
-</div>
+  <Topnav />
+  <div class="content">
+    <aside class="aside" v-if="asideVisible" style="border: 1px solid green">
+      <ol class="aside-list">
+        <li>
+          <router-link to="/doc/switch">Switch组件</router-link>
+        </li>
+        <li>
+          <router-link to="/doc/button">Button 组件</router-link>
+        </li>
+        <li>
+          <router-link to="/doc/dialog">Dialog 组件</router-link>
+        </li>
+        <li>
+          <router-link to="/doc/tabs">Tabs 组件</router-link>
+        </li>
+      </ol>
+    </aside>
+    <main class="main" style="border: 1px solid blue">
+      <router-view></router-view>
+    </main>
+  </div>
 </template>
 
 <script lang="ts">
-import Topnav from './../components/Topnav.vue'
-import {
-  inject,
-  Ref
-} from 'vue'
+import Topnav from "./../components/Topnav.vue";
+import { inject, Ref } from "vue";
 
 export default {
   setup() {
-    const asideVisible = inject < Ref < boolean >> ('asideVisible')
+    const asideVisible = inject<Ref<boolean>>("asideVisible");
     return {
-      asideVisible
-    }
+      asideVisible,
+    };
   },
 
   components: {
-    Topnav
-  }
-}
+    Topnav,
+  },
+};
 </script>
 
 <style lang="scss" scoped>
@@ -57,16 +54,16 @@ export default {
     width: 260px;
     box-sizing: border-box;
     padding: 20px 0 0 20px;
-    background: rgba(255, 255, 255, .9) url('./../assets/img/character_emilia.png') bottom/contain no-repeat;
+    background: rgba(255, 255, 255, 0.6) url("./../assets/img/character_emilia.gif")
+      bottom/contain no-repeat;
 
     .aside-list {
-      background-color: rgba(255, 255, 255, .8);
+      background-color: rgba(255, 255, 255, 0.8);
 
       li {
         &:not(:first-child) {
           margin-top: 0.5em;
         }
-
       }
     }
   }
@@ -76,6 +73,16 @@ export default {
     padding-top: 20px;
     padding-left: 20px;
     overflow: scroll;
+  }
+}
+
+@media (max-width: 900px) {
+  .content {
+    .aside {
+      position: absolute;
+      z-index: 99;
+      width: 260px;
+    }
   }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,69 +1,79 @@
 <template>
-<Topnav />
-<div class="home">
-  <h1 class="title">
-    <span class="left">Re</span>:Zero UI
-  </h1>
-  <div class="subtitle">基于 Vue.js 3.x<br>UI轮子库</div>
-  <div class="btn-wrapper">
-    <div class="btn" @click="$router.push('/doc')">快速开始</div>
+  <!-- <Topnav /> -->
+  <div class="home">
+    <div class="main">
+      <h1 class="title"><span class="left">Re</span>:Zero UI</h1>
+      <div class="subtitle">基于 Vue.js 3.x 的 UI 轮子库</div>
+      <div class="btn-wrapper">
+        <div class="btn" @click="$router.push('/doc')">快速开始</div>
+      </div>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
-import Topnav from './../components/Topnav.vue'
+// import Topnav from './../components/Topnav.vue'
 
 export default {
   components: {
-    Topnav
-  }
-}
+    // Topnav
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-@import './../assets/style/variable.scss';
+@import "./../assets/style/variable.scss";
 
 .home {
   flex-grow: 1;
   display: flex;
-  flex-direction: column;
+  // flex-direction: column;
   justify-content: center;
   align-items: center;
 
-  .title {
-    font-family: "Times New Roman", Times, Baskerville, Georgia, serif;
-    margin-top: 0;
-  }
+  .main {
+    padding: 5vw 0;
+    width: 45vw;
+    background-color: rgba(255, 255, 255, .6);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    .title {
+      font-family: "Times New Roman", Times, Baskerville, Georgia, serif;
+      margin-top: 0;
+    }
 
-  .left {
-    color: $colorEmilia;
-  }
-
-  .subtitle {
-    text-align: center;
-    font-size: 18px;
-    line-height: 1.5em;
-    margin-bottom: 1.5em;
-    color: #273849;
-  }
-
-  .btn-wrapper {
-    .btn {
-      border: 1px solid;
-      border-color: $colorEmilia;
-      background-color: transparent;
-      text-transform: uppercase;
-      font-size: 14px;
-      padding: 10px 20px;
-      font-weight: 400;
+    .left {
       color: $colorEmilia;
-      cursor: pointer;
+    }
 
-      &:hover {
-        color: #ffffff;
-        background-color: $colorEmilia;
-        box-shadow: 10px 10px 99px 6px $colorEmilia;
+    .subtitle {
+      text-align: center;
+      font-size: 18px;
+      line-height: 1.5em;
+      // margin-bottom: 1.5em;
+      margin: 1em 0;
+      color: #273849;
+    }
+
+    .btn-wrapper {
+      .btn {
+        border: 1px solid;
+        border-color: $colorEmilia;
+        background-color: transparent;
+        text-transform: uppercase;
+        font-size: 14px;
+        padding: 10px 20px;
+        font-weight: 400;
+        color: $colorEmilia;
+        cursor: pointer;
+
+        &:hover {
+          color: #ffffff;
+          background-color: $colorEmilia;
+          box-shadow: 10px 10px 99px 6px $colorEmilia;
+        }
       }
     }
   }
